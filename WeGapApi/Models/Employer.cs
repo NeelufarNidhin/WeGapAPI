ea2a7266a11709e9c453bd934e28d6e8e1ba5234
neelufarnidhin <nilu.nidhin@gmail.com>
2024-01-20 02:04:54 +0400

    Employer Added

--- WeGapApi/Models/Employer.cs
+++ WeGapApi/Models/Employer.cs
@@ -9,11 +9,16 @@ namespace WeGapApi.Models
 		public Guid Id { get; set; }
 		[Required (ErrorMessage = "Company Name is a required !!")]
         public string CompanyName { get; set; }
-		public Guid UserId { get; set; }
+        public string ApplicationUserId { get; set; }
         public string Location  { get; set; }
         public string Website { get; set; }
 		public string Description { get; set; }
 		public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+
+        //Navigation
+
+        public ApplicationUser ApplicationUser { get; set; }
+
     }
 }
 
